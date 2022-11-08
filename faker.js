0ee63b60b6da0fc331c8a38455b0c1094f69c919
mangoshi <soggymoose@gmail.com>
2022-11-08 00:29:17 +0000

    Improved faker script more
    - Now defaults to 1 user when NaN arguments are passed
    - Also added comments

--- faker.js
+++ faker.js
@@ -1,12 +1,15 @@
-// require the necessary libraries
+// Import necessary libraries
 const mongoose = require("mongoose");
 const { faker } = require("@faker-js/faker");
 require('dotenv').config();
 
+// Import user schema
 const User = require('./models/user_schema')
 
+// Select target database
 let selected_database = 'Datasets'
 
+// Connect to MongoDB using DB_ATLAS_URL from .env file, and target DB
 mongoose.connect(`${process.env.DB_ATLAS_URL}/${selected_database}`, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true
@@ -21,26 +24,49 @@ mongoose.connect(`${process.env.DB_ATLAS_URL}/${selected_database}`, {
 // console.log("1: ", process.argv[1])
 // console.log("2: ", process.argv[2])
 
-let count = process.argv[2]
+// Initialising count as the first argument passed on command-line
+// Converting to integer through parseInt() so that it is either a number or NaN
+let count = parseInt(process.argv[2])
 
+// Checking to see if count is an integer
+if(Number.isInteger(count)){
+	// If it is, log it and move on
+	console.log("Argument is an integer!")
+} else {
+	// If it isn't, warn user and set count to 1
+	console.log(`Argument is a ${typeof process.argv[2]}! An integer is required to define amount of users.`)
+	count = 1
+}
+console.log(`Generating ${count} user(s)!`)
+
+// Initialising fakedUsers empty array
 let fakedUsers = []
 
+// For loop defined by count (command-line argument)
 for(let i = 0; i < count; i++) {
 
+	// Initialising fakeUser Object
 	let fakeUser = {
+		// Generating username
 		username: faker.name.firstName(),
+		// Generating email based on username
 		email: faker.internet.email(this.username),
+		// Generating password // TODO: hash it!
 		password: faker.internet.password(),
+		// Generating role (picks from basic and admin)
 		role: faker.helpers.arrayElement(['basic', 'admin']),
 	}
+	// Push generated user to fakedUsers array
 	fakedUsers.push(fakeUser)
 }
 
-	let seedDB = async () => {
-		await User.insertMany(fakedUsers)
-	}
+// Using insertMany function to insert entire array into the database
+let seedDB = async () => {
+	await User.insertMany(fakedUsers)
+}
 
-	seedDB().then(()=> {
-		console.log("Operation successful!")
-		mongoose.connection.close()
-	})
+// After seeding, log success and close connection to database
+seedDB().then(()=> {
+	console.log("Operation successful!")
+	mongoose.connection.close()
+})
